c4ba3493cb56866c08676057fb813059bb14ddbe
shreyasvaid <shreyasvaid1@gmail.com>
2024-11-04 13:01:53 -0500

    Updating folders

--- Project_Manager_Tool/apps.py
+++ Project_Manager_Tool/apps.py
@@ -3,7 +3,7 @@ from django.apps import AppConfig
 
 class HelloConfig(AppConfig):  # Replace 'HelloConfig' with your app’s config class if different
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'hello'  # Make sure this matches your app name
+    name = 'Project_Manager_Tool'  # Make sure this matches your app name
 
     def ready(self):
-        import hello.signals  # Import your signals module here
+        import Project_Manager_Tool.signals  # Import your signals module here
